9551ff76a743a8c16f3cee2e22394043fc743f5c
teenage scumbag <vrysaint@gmail.com>
2026-02-23 21:41:59 +0700

    Delete plugins/custom-timestamps/src directory

--- plugins/custom-timestamps/src/ui/components/SelectableRow.tsx
+++
@@ -1,9 +0,0 @@
-import { findByName } from "@vendetta/metro";
-import { Forms } from "@vendetta/ui/components";
-
-const { FormRow } = Forms;
-const RowCheckmark = findByName("RowCheckmark");
-
-export function SelectableRow({ label, subLabel, selected, onPress }) {
-    return <FormRow label={label} subLabel={subLabel} trailing={<RowCheckmark selected={selected} />} onPress={onPress} />
-}
